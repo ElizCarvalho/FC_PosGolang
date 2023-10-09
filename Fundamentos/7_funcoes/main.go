0c8b81ba3c1cef29fdcf0010ebcdb4943fbaddfc
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2023-10-08 21:20:18 -0300

    functions class

--- Fundamentos/7_funcoes/main.go
+++ Fundamentos/7_funcoes/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"errors"
+	"fmt"
+)
 
+func main() {
+	fmt.Println(sum(3, 5))
+	result, err := sum(30, 50)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(result)
+}
+
+func sum(num1, num2 int) (int, error) {
+	if num1+num2 >= 50 {
+		return 0, errors.New("a soma é maior que 50")
+	}
+	return num1 + num2, nil
 }
