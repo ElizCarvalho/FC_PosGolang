2577d1c269a4a642cfac16490adc33790751e9c0
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2023-10-08 21:45:13 -0300

    funcoes variaticas

---
+++ Fundamentos/8_funcoes_variadicas/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(soma(0, 1, 1, 2, 3, 5, 8, 13, 21))
+}
+
+// ... diz que vai entrar um número variável de parâmetros, tds do mesmo tipo
+func soma(numbers ...int) int {
+	total := 0
+	for _, number := range numbers {
+		total += number
+	}
+	return total
+}
