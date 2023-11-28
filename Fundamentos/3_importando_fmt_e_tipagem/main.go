dcdc053c05623f4137f793d396bec090656e4a05
ElizCarvalho <elizabethcarvalh0@yahoo.com>
2023-11-28 15:07:43 -0300

    refactor: adjust format print

--- Fundamentos/3_importando_fmt_e_tipagem/main.go
+++ Fundamentos/3_importando_fmt_e_tipagem/main.go
@@ -14,8 +14,8 @@ var (
 
 func main() {
 	fmt.Printf("O tipo da variavel B é %T e o valor é %v \n", b, b)
-	fmt.Printf("O tipo da variavel C é %T e o valor é %v \n", c, c)
+	fmt.Printf("O tipo da variavel C é %T e o valor é %d \n", c, c)
 	fmt.Printf("O tipo da variavel D é %T e o valor é %v \n", d, d)
 	fmt.Printf("O tipo da variavel E é %T e o valor é %v \n", e, e)
-	fmt.Printf("O tipo da variavel F é %T e o valor é %v \n", f, f)
+	fmt.Printf("O tipo da variavel F é %T e o valor é %d \n", f, f)
 }
