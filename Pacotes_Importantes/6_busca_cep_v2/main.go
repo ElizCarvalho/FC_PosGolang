bdcd107a2c04a559ab32347e849b15761afd166e
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2024-02-05 22:04:50 -0300

    feature: add busca_cep_v2 lesson

---
+++ Pacotes_Importantes/6_busca_cep_v2/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type ViaCEP struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Unidade     string `json:"unidade"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+}
+
+func main() {
+	http.HandleFunc("/", BuscaCepHandler)
+	fmt.Println("Servidor escutando na porta 8080...")
+	http.ListenAndServe(":8080", nil)
+}
+
+func BuscaCepHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	cepParam := r.URL.Query().Get("cep")
+	if len(cepParam) != 8 {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("CEP inválido."))
+		return
+	}
+
+	cep, err := BuscaCep(cepParam)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Erro ao buscar o CEP."))
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(cep)
+}
+
+func BuscaCep(cep string) (*ViaCEP, error) {
+	resp, err := http.Get("https://viacep.com.br/ws/" + cep + "/json/")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var c ViaCEP
+	err = json.Unmarshal(body, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
