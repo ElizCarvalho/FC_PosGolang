ce39adfd35104f653a67e26f39d6a9a2003545ec
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-09-23 09:39:13 -0300

    feat(database): add Create and FindByEmail methods to UserDB

--- 7_APIS/infra/database/user_db.go
+++ 7_APIS/infra/database/user_db.go
@@ -1,6 +1,9 @@
 package database
 
-import "gorm.io/gorm"
+import (
+	"github.com/ElizCarvalho/FC_PosGolang/7_APIS/internal/entity"
+	"gorm.io/gorm"
+)
 
 type UserDB struct {
 	db *gorm.DB
@@ -8,4 +11,16 @@ type UserDB struct {
 
 func NewUserDB(db *gorm.DB) *UserDB {
 	return &UserDB{db: db}
+}
+
+func (u *UserDB) Create(user *entity.User) error {
+	return u.db.Create(user).Error
+}
+
+func (u *UserDB) FindByEmail(email string) (*entity.User, error) {
+	var user entity.User
+	if err := u.db.Where("email = ?", email).First(&user).Error; err != nil {
+		return nil, err
+	}
+	return &user, nil
 }
