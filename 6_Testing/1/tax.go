7be26dd65f4d8e769b8ba4b3a96576a729d77c40
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-09-22 22:09:47 -0300

    docs(tax): add coverage instructions for tax calculation tests

--- 6_Testing/1/tax.go
+++ 6_Testing/1/tax.go
@@ -1,6 +1,13 @@
 package tax
 
 func CalculateTax(amount float64) float64 {
+
+	//pra conseguir ver o quanto de cobertura temos
+	//usamos o comando $ go test -v -coverprofile=coverage.out (pra criar o arquivo de cobertura)
+	//e depois usamos o comando $ go tool cover -html=coverage.out (pra ver a cobertura)
+	if amount == 0 {
+		return 0
+	}
 	if amount >= 1000 {
 		return 10.0
 	}
