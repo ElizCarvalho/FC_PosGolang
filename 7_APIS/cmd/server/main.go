782634ddb63433658befea1a8493ee7821a83208
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-04 17:53:04 -0300

    feat(api): add user creation endpoint

--- 7_APIS/cmd/server/main.go
+++ 7_APIS/cmd/server/main.go
@@ -26,6 +26,8 @@ func main() {
 	db.AutoMigrate(&entity.Product{})
 	productDB := database.NewProductDB(db)
 	productHandler := handlers.NewProductHandler(productDB)
+	userDB := database.NewUserDB(db)
+	userHandler := handlers.NewUserHandler(userDB)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
@@ -34,6 +36,8 @@ func main() {
 	r.Get("/products/{id}", productHandler.GetProduct)
 	r.Put("/products/{id}", productHandler.UpdateProduct)
 	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	r.Post("/users", userHandler.CreateUser)
 
 	http.ListenAndServe(":"+config.WebServerPort, r)
 }
