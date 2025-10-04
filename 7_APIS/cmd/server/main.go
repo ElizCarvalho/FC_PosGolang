60e3c28660c2fcdce9179a8d37feb6f6094f6d3e
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-04 19:09:17 -0300

    feat(api): secure product and user endpoints with JWT authentication

--- 7_APIS/cmd/server/main.go
+++ 7_APIS/cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ElizCarvalho/FC_PosGolang/7_APIS/internal/webserver/handlers"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/jwtauth"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -31,14 +32,21 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
 
-	r.Post("/users", userHandler.CreateUser)
-	r.Post("/users/generate_token", userHandler.GetJWT)
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth))
+		r.Use(jwtauth.Authenticator)
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.CreateUser)
+		r.Post("/generate_token", userHandler.GetJWT)
+	})
 
 	http.ListenAndServe(":"+config.WebServerPort, r)
 }
