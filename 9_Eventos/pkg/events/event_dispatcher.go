92ce1e2d230b0492b3625a499cde669debef3761
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-05 21:32:24 -0300

    feat(events): implement event dispatcher for handling events

---
+++ 9_Eventos/pkg/events/event_dispatcher.go
@@ -0,0 +1,34 @@
+package events
+
+import (
+	"errors"
+	"slices"
+)
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if slices.Contains(ed.handlers[eventName], handler) {
+		return ErrHandlerAlreadyRegistered
+	}
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	return slices.Contains(ed.handlers[eventName], handler)
+}
