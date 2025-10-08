1776bbc4ed00a7d51a73be2dcb360877bd7425d3
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-08 06:46:11 -0300

    feat(consumer): implement RabbitMQ message consumer to receive and acknowledge messages

---
+++ 9_Eventos/cmd/consumer/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ElizCarvalho/fcutils/rabbitmq"
+	"github.com/streadway/amqp"
+)
+
+func main() {
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgs := make(chan amqp.Delivery)
+	err = rabbitmq.Consume(ch, msgs)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("🎧 Aguardando mensagens...")
+	for msg := range msgs {
+		fmt.Println("📨 Mensagem recebida:", string(msg.Body))
+		msg.Ack(false) // requeue false para não reenviar a mensagem
+	}
+}
