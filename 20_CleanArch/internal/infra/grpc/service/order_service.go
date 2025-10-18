c0a1ed85de6be571ae2006f22cd51fbd22732184
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-18 17:54:02 -0300

    feat(grpc): add ListOrders RPC method

--- 20_CleanArch/internal/infra/grpc/service/order_service.go
+++ 20_CleanArch/internal/infra/grpc/service/order_service.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 
+	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/internal/entity"
 	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/internal/infra/grpc/pb"
 	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/internal/usecase"
 )
@@ -10,11 +11,13 @@ import (
 type OrderService struct {
 	pb.UnimplementedOrderServiceServer
 	CreateOrderUseCase usecase.CreateOrderUseCase
+	OrderRepository    entity.OrderRepositoryInterface
 }
 
-func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase) *OrderService {
+func NewOrderService(createOrderUseCase usecase.CreateOrderUseCase, orderRepository entity.OrderRepositoryInterface) *OrderService {
 	return &OrderService{
 		CreateOrderUseCase: createOrderUseCase,
+		OrderRepository:    orderRepository,
 	}
 }
 
@@ -33,5 +36,27 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Price:      float32(output.Price),
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.ListOrdersRequest) (*pb.ListOrdersResponse, error) {
+	listOrdersUseCase := usecase.NewListOrdersUseCase(s.OrderRepository)
+	orders, err := listOrdersUseCase.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	var pbOrders []*pb.Order
+	for _, order := range orders {
+		pbOrders = append(pbOrders, &pb.Order{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		})
+	}
+
+	return &pb.ListOrdersResponse{
+		Orders: pbOrders,
 	}, nil
 }
