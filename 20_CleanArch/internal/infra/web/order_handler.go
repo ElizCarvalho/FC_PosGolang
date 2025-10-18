dca9052970a984be94b3a9d7bdd1d73b5a023964
Elis Carvalho <elizabethcarvalh0@yahoo.com>
2025-10-18 15:32:28 -0300

    feat(web): add Order HTTP handler

---
+++ 20_CleanArch/internal/infra/web/order_handler.go
@@ -0,0 +1,49 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/events"
+	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/internal/entity"
+	"github.com/ElizCarvalho/FC_PosGolang/20_CleanArch/internal/usecase"
+)
+
+type WebOrderHandler struct {
+	EventDispatcher   events.EventDispatcherInterface
+	OrderRepository   entity.OrderRepositoryInterface
+	OrderCreatedEvent events.EventInterface
+}
+
+func NewWebOrderHandler(
+	EventDispatcher events.EventDispatcherInterface,
+	OrderRepository entity.OrderRepositoryInterface,
+	OrderCreatedEvent events.EventInterface,
+) *WebOrderHandler {
+	return &WebOrderHandler{
+		EventDispatcher:   EventDispatcher,
+		OrderRepository:   OrderRepository,
+		OrderCreatedEvent: OrderCreatedEvent,
+	}
+}
+
+func (h *WebOrderHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var dto usecase.OrderInputDTO
+	err := json.NewDecoder(r.Body).Decode(&dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	createOrder := usecase.NewCreateOrderUseCase(h.OrderRepository, h.OrderCreatedEvent, h.EventDispatcher)
+	output, err := createOrder.Execute(dto)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = json.NewEncoder(w).Encode(output)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
